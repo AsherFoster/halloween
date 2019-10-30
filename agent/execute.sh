f623fcb1ebc276300bf89eed20f5455e749ef2cb
Asher Foster <me@asherfoster.com>
2019-10-30 23:14:42 +1300

    Added launchd

--- agent/execute.sh
+++ agent/execute.sh
@@ -1,7 +1,13 @@
 URL=https://asherfoster.com/halloween/client/index.html
 
-if [ -d "/Applications/Google\ Chrome.app" ]; then
-  open /Applications/Google\ Chrome.app --args $URL
+osascript -e "set Volume 10"
+
+if [ -d "/Applications/Google Chrome.app" ]; then
+  if [ $(ps -ax | grep Google\ Chrome | wc -l | xargs) == 1 ]; then
+    open /Applications/Google\ Chrome.app --args --start-fullscreen $URL
+  else
+    open -a "google chrome" $URL
+  fi
 else
   open -a safari $URL
 fi
