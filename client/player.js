205ddd1059b244eec76adf07e99ddcf20266aefa
Asher Foster <me@asherfoster.com>
2019-11-01 11:19:51 +1300

    Muted

--- client/player.js
+++ client/player.js
@@ -3,10 +3,10 @@
   window.DEBUG_INFO = {};
   let player;
   let ready = false;
-  let muted = location.search === '?muted';
+  let muted = true || location.search === '?muted';
 
   const videoEl = document.getElementById('video');
-  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&origin=' + window.location.origin;
+  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
 
   window.onYouTubeIframeAPIReady = () => {
     console.log('YT API Ready');
@@ -21,21 +21,21 @@
 
   function onPlayerReady() {
     console.log('Player ready', ready);
-    // if (muted) player.setVolume(0);
-    // player.playVideo();
-    // if (!ready) { // Make it pause once it's buffered a little if we aren't playing
-    //   setTimeout(() => {
-    //     player.pauseVideo();
-    //     player.seekTo(1, true);
-    //   }, 100);
-    // } else {
-    //   ready = true
-    // }
+    if (muted) player.setVolume(0);
+    player.playVideo();
+    if (!ready) { // Make it pause once it's buffered a little if we aren't playing
+      setTimeout(() => {
+        player.pauseVideo();
+        player.seekTo(1, true);
+      }, 100);
+    } else {
+      ready = true
+    }
   }
   window.countdownComplete = () => {
     console.log('Countdown complete', ready);
-    // if (ready)
+    if (ready)
       player.playVideo();
-    // ready = true
+    ready = true
   }
 })();
