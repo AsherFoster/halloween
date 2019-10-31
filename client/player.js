dcf2ec7ff461eeff75bba8d492a084cfced0add5
Asher Foster <me@asherfoster.com>
2019-11-01 11:03:35 +1300

    Change video because copyright?

--- client/player.js
+++ client/player.js
@@ -6,13 +6,12 @@
   let muted = location.search === '?muted';
 
   const videoEl = document.getElementById('video');
-  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
+  videoEl.src = 'https://www.youtube.com/embed/Z_UOmAed5O8?enablejsapi=1&controls=0&origin=' + window.location.origin;
 
   window.onYouTubeIframeAPIReady = () => {
     console.log('YT API Ready');
     player = new YT.Player('video', {
-      videoId: 'dQw4w9WgXcQ', // Take a guess
-      playerVars: {controls: 0},
+      videoId: 'Z_UOmAed5O8', // Take a guess
       events: {
         onReady: onPlayerReady,
       }
