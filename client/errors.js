f6b8ef6b523d44ab58fd0a48dd8191a0e5626f77
Asher Foster <me@asherfoster.com>
2019-10-31 13:58:29 +1300

    Logs

--- client/errors.js
+++ client/errors.js
@@ -1,6 +1,7 @@
 (function () {
   window.errors = {
     audioFail(error, name) {
+      logs.textContent += error;
       if (error.message.includes('user didn\'t interact') ||
         error.message.includes('play method is not allowed by the user agent')) {
         return console.warn('Autoplay prevented');
