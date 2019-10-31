696546bbe554e3776e5619c151fa94b4c50f0f20
Asher Foster <me@asherfoster.com>
2019-11-01 11:11:20 +1300

    Remove buffering code

--- client/player.js
+++ client/player.js
@@ -6,12 +6,12 @@
   let muted = location.search === '?muted';
 
   const videoEl = document.getElementById('video');
-  videoEl.src = 'https://www.youtube.com/embed/Z_UOmAed5O8?enablejsapi=1&controls=0&origin=' + window.location.origin;
+  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
 
   window.onYouTubeIframeAPIReady = () => {
     console.log('YT API Ready');
     player = new YT.Player('video', {
-      videoId: 'Z_UOmAed5O8', // Take a guess
+      videoId: 'dQw4w9WgXcQ', // Take a guess
       events: {
         onReady: onPlayerReady,
       }
@@ -21,20 +21,21 @@
 
   function onPlayerReady() {
     console.log('Player ready', ready);
-    if (muted) player.setVolume(0);
-    player.playVideo();
-    if (!ready) { // Make it pause once it's buffered a little if we aren't playing
-      setTimeout(() => {
-        player.pauseVideo();
-        player.seekTo(1, true);
-      }, 100);
-    } else {
-      ready = true
-    }
+    // if (muted) player.setVolume(0);
+    // player.playVideo();
+    // if (!ready) { // Make it pause once it's buffered a little if we aren't playing
+    //   setTimeout(() => {
+    //     player.pauseVideo();
+    //     player.seekTo(1, true);
+    //   }, 100);
+    // } else {
+    //   ready = true
+    // }
   }
   window.countdownComplete = () => {
     console.log('Countdown complete', ready);
-    if (ready) player.playVideo();
-    ready = true
+    // if (ready)
+      player.playVideo();
+    // ready = true
   }
 })();
