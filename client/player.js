b2025fa01510779c8ac1abf2cd3f9e37edca4b40
Asher Foster <me@asherfoster.com>
2019-11-01 13:33:01 +1300

    Back to youtube

--- client/player.js
+++ client/player.js
@@ -6,22 +6,22 @@
   let muted = location.search === '?muted';
 
   const videoEl = document.getElementById('video');
-  // videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
+  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
 
   window.onYouTubeIframeAPIReady = () => {
     console.log('YT API Ready');
-    player = new Vimeo.Player(videoEl, {controls: false});
-    player.playVideo = () => player.play();
-    player.pauseVideo = () => player.pause();
-    player.seekTo = (n) => player.setCurrentTime(n);
-    onPlayerReady();
-    // player = new YT.Player('video', {
-    //   videoId: 'dQw4w9WgXcQ', // Take a guess
-    //   events: {
-    //     onReady: onPlayerReady,
-    //   }
-    // });
-    // window.player = player;
+    // player = new Vimeo.Player(videoEl, {controls: false});
+    // player.playVideo = () => player.play();
+    // player.pauseVideo = () => player.pause();
+    // player.seekTo = (n) => player.setCurrentTime(n);
+    // onPlayerReady();
+    player = new YT.Player('video', {
+      videoId: 'dQw4w9WgXcQ', // Take a guess
+      events: {
+        onReady: onPlayerReady,
+      }
+    });
+    window.player = player;
   };
   onYouTubeIframeAPIReady();
 
