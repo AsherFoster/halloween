ed5c02da2c22f11d7e7ebd5dd137813d49c5e278
Asher Foster <me@asherfoster.com>
2019-11-01 13:50:07 +1300

    Change iframe to div

--- client/player.js
+++ client/player.js
@@ -5,10 +5,9 @@
   let ready = false;
   let muted = location.search === '?muted';
 
-  const videoEl = document.getElementById('video');
-  videoEl.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?enablejsapi=1&controls=0&origin=' + window.location.origin;
-
+  // const videoEl = document.getElementById('video');
   window.onYouTubeIframeAPIReady = () => {
+
     console.log('YT API Ready');
     // player = new Vimeo.Player(videoEl, {controls: false});
     // player.playVideo = () => player.play();
@@ -19,6 +18,9 @@
       videoId: 'dQw4w9WgXcQ', // Take a guess
       events: {
         onReady: onPlayerReady,
+      },
+      playerVars: {
+        controls: 0
       }
     });
     window.player = player;
@@ -28,13 +30,13 @@
     console.log('Player ready', ready);
     if (muted) player.setVolume(0);
     if (!ready) { // Make it pause once it's buffered a little if we aren't playing
-      player.playVideo().then(() => {
+      // player.playVideo().then(() => {
+      //
+      // });
+      setTimeout(() => {
         player.pauseVideo();
         player.seekTo(1, true);
-
-      });
-      // setTimeout(() => {
-      // }, 1000);
+      }, 500);
     } else {
       player.playVideo();
     }
